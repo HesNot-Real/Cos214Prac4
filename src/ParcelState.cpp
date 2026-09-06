3786a07fc4ef6673a12e7512e3e18dedf944e50e
devuser <lushomo88@gmail.com>
2026-09-06 15:57:55 +0200

    fixing parcelState

--- src/ParcelState.cpp
+++ src/ParcelState.cpp
@@ -1,14 +1,1 @@
-#ifndef AWAITINGPICKUPSTATE_H
-#define AWAITINGPICKUPSTATE_H
-
 #include "ParcelState.h"
-
-class AwaitingPickupState : public ParcelState {
-
-public:
-	void advance(Parcel* context);
-	void reportLost(Parcel* context);
-	string getStatusName();
-};
-
-#endif
